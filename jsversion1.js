44966b7f9e2170932688441d333c301a2e55e56a
shelbyreese47 <shelbyreese47@gmail.com>
2021-08-27 14:34:24 -0600

    Basic MVP done

--- jsversion1.js
+++ jsversion1.js
@@ -33,7 +33,7 @@ const words = [
 	'SPACE',
 	'COOKIES',
 	'YELLOW',
-	'COOKIES',
+	'PIE',
 ];
 
 // Player 1
@@ -50,14 +50,22 @@ let wordLength;
 // The current phrase/word that has been selected
 // Randomly choose a phrase/word
 // let currentWord = words[Math.ceil(Math.random() * 10) - 1];
-let currentWord = words[Math.ceil(Math.random() * 10) - 1];
-console.log(currentWord);
+// let currentWord = words[Math.ceil(Math.random() * 10) - 1];
+let currentWord;
+// let currentWord = pickNewWord();
 
 /*---------------------------------------FUNCTIONS-----------------------------------------*/
+let unusedWordArray = words;
+pickNewWord();
 
-// Randomly choose a phrase/word
+console.log(currentWord);
+
+// Randomly choose a phrase/word AND make sure that word is not picked again
 function pickNewWord() {
-	currentWord = words[Math.ceil(Math.random() * 10) - 1];
+	currentWord =
+		unusedWordArray[Math.ceil(Math.random() * unusedWordArray.length) - 1];
+	let currentWordIndex = unusedWordArray.indexOf(currentWord);
+	unusedWordArray.splice(currentWordIndex, 1);
 	return currentWord;
 }
 
@@ -69,7 +77,6 @@ function dashes(word) {
 	// console.log(wordLength);
 	// Create one dash per letter in the array
 	array.forEach((element) => {
-		console.log(element);
 		let div = document.createElement('div');
 		div.classList.add('dashes', element);
 		flexDown.appendChild(div);
@@ -84,11 +91,9 @@ function compareLetters(word, letter, clickedSquare) {
 		//Going to need a loop to check each letter and return it's index
 		// console.log(word.indexOf(letter));
 		const emptyDash = document.querySelectorAll(`.${letter}`);
-		console.log(emptyDash);
 		for (let i = 0; i < emptyDash.length; i++) {
 			emptyDash[i].innerText = letter;
 			winArray.push('1'); //this means when winArray.length = emptyDash.length - the player wins
-			console.log(winArray.length);
 		}
 	} else {
 		clickedSquare.classList.add('wrong');
@@ -104,39 +109,44 @@ function compareLetters(word, letter, clickedSquare) {
 // Check Win (needs to be called after each turn)
 function checkWin() {
 	if (winArray.length === wordLength) {
-		console.log('WINNER');
 		gameCount++;
 		score += 10;
-		console.log(gameCount);
-		console.log(score);
-		winner();
-		pickNewWord();
-		console.log(currentWord);
-		dashes(currentWord);
+		setTimeout(resetBoard, 1000); //Wanted a delay after the last letter is selected
 		return;
 	} else {
 		return;
 	}
 }
+function checkLose() {
+	if (chancesLeft === 0) {
+		console.log('lose');
+		resetBoard();
+	}
+}
 
-function winner() {
+function resetBoard() {
 	let div = document.querySelectorAll('.dashes');
 	for (let i = 0; i < wordLength; i++) {
 		flexDown.removeChild(div[i]);
 	}
 	let clickedLetters = document.querySelectorAll('.clicked');
-	console.log(clickedLetters);
 	for (let i = 0; i < clickedLetters.length; i++) {
 		clickedLetters[i].classList.remove('clicked');
 	}
-    let wrongLetters = document.querySelectorAll('.wrong');
-		console.log(clickedLetters);
-		for (let i = 0; i < clickedLetters.length; i++) {
-			clickedLetters[i].classList.remove('clicked');
-		}
+	let wrongLetters = document.querySelectorAll('.wrong');
+	for (let i = 0; i < wrongLetters.length; i++) {
+		wrongLetters[i].classList.remove('wrong');
+	}
+	winArray = [];
+	minionImage.setAttribute('src', '');
+	chancesLeft = 7;
 	scoreEl.innerText = `Score: ${score}`;
 	gameCountEl.innerText = `Game Count: ${gameCount}`;
+	pickNewWord();
+	console.log(currentWord);
+	dashes(currentWord);
 }
+
 //EVENT LISTENERS//
 // Click on a letter
 flex.addEventListener('click', (event) => {
@@ -151,6 +161,7 @@ flex.addEventListener('click', (event) => {
 		compareLetters(currentWord, letter, clickedSquare);
 		event.target.classList.add('clicked');
 		checkWin();
+		checkLose();
 	} else {
 		return 1;
 	}
