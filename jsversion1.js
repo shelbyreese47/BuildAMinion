9a1997bf2d39ec2b43c6afe6badb14478dfb99dc
shelbyreese47 <shelbyreese47@gmail.com>
2021-08-27 18:12:24 -0600

    Adding a second level

--- jsversion1.js
+++ jsversion1.js
@@ -7,7 +7,7 @@ const flex = document.querySelector('.flex-container');
 const minionImage = document.querySelector('.minionImage');
 const chancesLeftBoard = document.querySelector('.chancesLeft');
 const flexDown = document.querySelector('.flex-down');
-const gameCountEl = document.querySelector('.gameCount');
+const levelEl = document.querySelector('.level');
 const scoreEl = document.querySelector('.score');
 
 const minionStep = [
@@ -19,7 +19,15 @@ const minionStep = [
 	'https://i.ibb.co/pfTL05g/Minion-Step6.png',
 	'https://i.ibb.co/RgtQ0cL/Minion-Step7.png',
 ];
-
+const evilStep = [
+	'https://i.ibb.co/S3TcDvC/evil-Step1.png',
+	'https://i.ibb.co/zPCF25v/evil-Step2.png',
+	'https://i.ibb.co/NmBNybW/evil-Step3.png',
+	'https://i.ibb.co/7W9Bx6C/evil-Step4.png',
+	'https://i.ibb.co/s5qrYkK/evil-Step5.png',
+	'https://i.ibb.co/Yyb9Hw7/evil-Step6.png',
+	'https://i.ibb.co/F8F9kQv/evil-Step7.png',
+];
 /*-----------------------------------CONSTANT VARIABLES------------------------------------*/
 
 // Phrases/words the computer can pick from
@@ -32,9 +40,11 @@ const words = [
 	'SHIP',
 	'SPACE',
 	'COOKIES',
-	'YELLOW',
-	'PIE',
+	'CHEETO',
+	'MOON',
 ];
+const secondLevelWords = ['ME WANT BANANA', 'SHRINK RAY', ''];
+const thirdLevelWords = ['ITS SO FLUFFY IM GONNA DIE'];
 
 // Player 1
 /*------------------------------------STATE VARIABLES-------------------------------------*/
@@ -44,6 +54,7 @@ let score = 0;
 let gameCount = 0;
 // How much of the minion is on the screen
 let chancesLeft = 7;
+let level = 1;
 
 let winArray = []; //We'll add an element to this array with every correct letter
 let wordLength;
@@ -72,9 +83,18 @@ function pickNewWord() {
 // Display dashes on the screen
 function dashes(word) {
 	let array = word.split('');
-	// console.log(array);
-	wordLength = array.length;
-	// console.log(wordLength);
+	let howManySpaces = [];
+	console.log(array);
+	array.forEach((element, index) => {
+		if (element === ' ') {
+			array.splice(index, 1, '_');
+			howManySpaces.push('1'); //Will add one number for each space
+		} else return;
+	});
+	console.log(howManySpaces);
+	dashesLength = array.length;
+	wordLength = array.length - howManySpaces.length;
+	console.log(wordLength);
 	// Create one dash per letter in the array
 	array.forEach((element) => {
 		let div = document.createElement('div');
@@ -99,8 +119,13 @@ function compareLetters(word, letter, clickedSquare) {
 		clickedSquare.classList.add('wrong');
 		chancesLeft--;
 		let steps = 6 - chancesLeft;
-		let image = minionStep[steps];
-		minionImage.setAttribute('src', image);
+		if (level === 1) {
+			let image = minionStep[steps];
+			minionImage.setAttribute('src', image);
+		} else if (level === 2) {
+			let image = evilStep[steps];
+			minionImage.setAttribute('src', image);
+		}
 		chancesLeftBoard.innerText = `Chances Left: ${chancesLeft}`;
 	}
 	return chancesLeft;
@@ -110,13 +135,35 @@ function compareLetters(word, letter, clickedSquare) {
 function checkWin() {
 	if (winArray.length === wordLength) {
 		gameCount++;
-		score += 10;
+		scoreChange();
+		setTimeout(winGame, 1000);
 		setTimeout(resetBoard, 1000); //Wanted a delay after the last letter is selected
 		return;
 	} else {
 		return;
 	}
 }
+function scoreChange() {
+	// I want the score to reflect how many letters the player guessed right vs wrong
+	let clickedLetters = document.querySelectorAll('.clicked');
+	let wrongGuesses = document.querySelectorAll('.wrong');
+	let rigthGuesses = clickedLetters.length - wrongGuesses.length;
+	score += 2 * rigthGuesses;
+	score -= 1 * wrongGuesses.length;
+	return score;
+}
+function winGame() {
+	if (gameCount >= 1) {
+		level = 2;
+		levelEl.innerText = `Level: 0${level}`;
+		// alert('SECOND LEVEL'); //--------------------------ADD A SECOND LEVEL
+		unusedWordArray = secondLevelWords;
+		return unusedWordArray;
+	}
+	console.log(unusedWordArray);
+	return unusedWordArray;
+}
+
 function checkLose() {
 	if (chancesLeft === 0) {
 		console.log('lose');
@@ -126,7 +173,7 @@ function checkLose() {
 
 function resetBoard() {
 	let div = document.querySelectorAll('.dashes');
-	for (let i = 0; i < wordLength; i++) {
+	for (let i = 0; i < dashesLength; i++) {
 		flexDown.removeChild(div[i]);
 	}
 	let clickedLetters = document.querySelectorAll('.clicked');
@@ -141,7 +188,7 @@ function resetBoard() {
 	minionImage.setAttribute('src', '');
 	chancesLeft = 7;
 	scoreEl.innerText = `Score: ${score}`;
-	gameCountEl.innerText = `Game Count: ${gameCount}`;
+
 	pickNewWord();
 	console.log(currentWord);
 	dashes(currentWord);
